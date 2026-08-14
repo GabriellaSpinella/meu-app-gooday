c80e94be39e44a180efd49040c1d2cec021d02c9
GabriellaSpinella <gabrielladsperez@gmail.com>
2026-08-14 12:23:57 -0300

    Add files via upload

---
+++ CadastroScreen.js
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Ionicons, TouchableOpacity, Image} from "react-native";
+import LoginScreen from "./LoginScreen";
+
+const CadastroScreen = ({navigation}) => {
+
+  const [email, setEmail] = useState('');
+  const [criar, setCriar] = useState('');
+  const [repetir, setRepetir] = useState('');
+  const [error, setError] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorRepetir, setErrorRepetir] = useState('');
+  const [errorCriar, setErrorCriar] = useState('');
+
+  function verificar() {
+
+    if(criar === '') {
+        setErrorCriar('É preciso digitar uma senha');
+      } else {
+        setErrorCriar('');
+      }
+
+    if(repetir === '') {
+      setErrorRepetir('É preciso digitar uma senha');
+      } else if (repetir !== criar && criar !== '') {
+          setErrorRepetir('As senhas devem ser iguais');
+          setErrorCriar('As senhas devem ser iguais');
+        } else {
+          setErrorRepetir('');
+        }
+
+    if (email === '') {
+        setErrorEmail('É preciso digitar um e-mail');
+      } else {
+        setErrorEmail('');
+      }
+  };
+
+    return(
+      <View style={styles.login}>
+
+        <Text style={styles.texto}>Cadastre-se</Text>
+        <Text style={styles.texto1}>Informe seu E-mail e crie uma senha</Text>
+
+        <Text style={styles.texto2}>E-mail</Text>
+        <TextInput style={styles.input}
+          placeholder="Digite seu E-mail"
+          placeholderTextColor='gray'
+          onChangeText={setEmail}
+          value={email}
+        />
+
+        {errorEmail ? <Text style={styles.erro}>{errorEmail}</Text> : null}
+
+        <Text style={styles.texto2}>Crie uma senha</Text>
+        <TextInput style={styles.input}
+          placeholder="Digite sua senha"
+          placeholderTextColor='gray'
+          onChangeText={setCriar}
+          value={criar}
+          secureTextEntry = {true}
+        />
+
+      {errorCriar ? <Text style={styles.erro}>{errorCriar}</Text> : null}
+      {error ? <Text style={styles.erro}>{error}</Text> : null}
+
+        <Text style={styles.texto2}>Repita a senha</Text>
+        <TextInput style={styles.input}
+          placeholder="Repita sua senha"
+          placeholderTextColor='gray'
+          onChangeText={setRepetir}
+          value={repetir}
+          secureTextEntry = {true}
+        />
+
+        {errorRepetir ? <Text style={styles.erro}>{errorRepetir}</Text> : null}
+        {error ? <Text style={styles.erro}>{error}</Text> : null}
+
+        <TouchableOpacity style={styles.btn} onPress={verificar}>
+          <Text style={styles.texto4}>Cadastrar</Text>
+        </TouchableOpacity>
+
+        <View style={styles.linhaContainer}>
+          <View style={styles.linha}></View>
+          <Text style={styles.texto5}>Ou continue com</Text>
+          <View style={styles.linha}></View>
+        </View>
+
+        <View style={styles.iconsContainer}>
+          <Image style={styles.icons} source={require('../img/google.png')} />
+          <Image style={styles.icons} source={require('../img/facebook.png')} />
+        </View>
+
+      </View>
+    );
+}
+
+export default CadastroScreen;
+
+const styles = StyleSheet.create({
+  login: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingHorizontal: 30,
+  },
+
+  input: {
+    backgroundColor: '#eeeeeeab',
+    borderWidth: 1,
+    borderColor:'#eeeeeeab',
+    borderRadius: 5,
+    width: '100%',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    textAlign: 'left',
+  },
+  texto: {
+    fontSize: 33,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  texto1: {
+    fontSize: 11,
+  },
+  texto2: {
+    marginTop: 20,
+  },
+  texto3: {
+    textAlign: 'right',
+    width: '100%',
+    marginTop: 20,
+  },
+  texto4: {
+    color: '#fff',
+  },
+  btn: {
+    backgroundColor: '#00e47a',
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    marginTop: 90,
+  },
+  linhaContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 45,
+  },
+  linha: {
+    flex: 1,
+    height: 1,
+    backgroundColor: '#808080',
+  },
+  texto5: {
+    marginHorizontal: 10,
+    color: '#808080',
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 20,
+    marginTop: 20,
+  },
+  icons: {
+    width: 50,
+    height: 50,
+  },
+  seta: {
+    width: 20,
+    height: 20,
+    marginTop: 30,
+  },
+  erro:{
+    color: 'red',
+  },
+});
---
+++ SecondScreen.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+export default function SecondScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../img/casual_dog.png')}
+        style={{ width: 280, height: 180, borderRadius: 40, resizeMode: 'contain'}}
+      />
+      <Text style={styles.title}>Olá! Como deseja acessar?</Text>
+
+      <TouchableOpacity style={styles.googleButton}>
+        <Image
+          source={require('../img/google.png')}
+          style={styles.googleIcon}
+        />
+        <Text style={styles.googleText}>Entrar com Google</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.otherButton} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.otherText}>Outras opções</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#010614',
+        marginTop: 50,
+    },
+    subtitle: {
+        fontSize: 15,
+        color: '#333',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    googleButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#2cd96b',
+        paddingVertical: 8,
+        width: '80%',
+        borderRadius: 10,
+        justifyContent: 'center',
+        marginBottom: 15,
+    },
+    googleIcon: {
+        width: 24,
+        height: 24,
+        marginRight: 10,
+    },
+    googleText: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    otherButton: {
+        borderWidth: 1,
+        borderColor: '#2cd96b',
+        paddingVertical: 8,
+        width: '80%', 
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    otherText: {
+        color: '#2cd96b',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
